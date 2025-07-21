1a5b0def68e663af952aff2d09cef19e673f57f3
DuongPKSE180524 <66048953+ReshTheDragon@users.noreply.github.com>
2025-07-21 15:38:13 +0700

    git push

--- src/api/orderAPI.js
+++ src/api/orderAPI.js
@@ -4,12 +4,14 @@ import apiClient from './api';
  * Fetch the order history for the current user.
  * @returns {Promise<Array>} Array of order objects
  */
-export const getOrderHistory = async () => {
+
+//Lấy lịch sử Order của khách hàng dựa trên id khách hàng
+export const getOrderHistory = async (customerId) => {
   try {
-    const response = await apiClient.get('/orders/history');
+    const response = await apiClient.get(`/Order/customer/${customerId}`);
     return response.data;
   } catch (error) {
-    console.error('Error fetching order history:', error);
+    console.error(`Error fetching order history for ${customerId}:`, error);
     throw error;
   }
 }; 
